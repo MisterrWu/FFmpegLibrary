a65f1da033782594f180c72185a0648630e1a3e5
wuhan <han.wu-app@lumiunited.com>
2019-11-03 21:07:04 +0800

    Feature: edit OUTPUTPATH to android_libs

--- android_libs/ffmpeg-build.sh
+++ android_libs/ffmpeg-build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-OUTPUTPATH=/Documents/works/gits/Android/FFmpegLibrary/android_libs
+OUTPUTPATH=android_libs
 NDK=/Users/wuhan/Library/Android/sdk/ndk
-TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
+TOOLCHAIN=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64
 API=29
 
 function build_android
 {
 echo "Compiling FFmpeg for $CPU"
 ./configure \
-    --prefix=$PREFIX \
+    --prefix=${PREFIX} \
     --disable-neon \
     --disable-hwaccels \
     --disable-gpl \
@@ -26,63 +26,63 @@ echo "Compiling FFmpeg for $CPU"
     --disable-avdevice \
     --disable-doc \
     --disable-symver \
-    --cross-prefix=$CROSS_PREFIX \
+    --cross-prefix=${CROSS_PREFIX} \
     --target-os=android \
-    --arch=$ARCH \
-    --cpu=$CPU \
-    --cc=$CC
-    --cxx=$CXX
+    --arch=${ARCH} \
+    --cpu=${CPU} \
+    --cc=${CC}
+    --cxx=${CXX}
     --enable-cross-compile \
-    --sysroot=$SYSROOT \
-    --extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
-    --extra-ldflags="$ADDI_LDFLAGS" \
-    $ADDITIONAL_CONFIGURE_FLAG
+    --sysroot=${SYSROOT} \
+    --extra-cflags="-Os -fpic ${OPTIMIZE_CFLAGS}" \
+    --extra-ldflags="${ADDI_LDFLAGS}" \
+    ${ADDITIONAL_CONFIGURE_FLAG}
 make clean
 make
 make install
-echo "The Compilation of FFmpeg for $CPU is completed"
+echo "The Compilation of FFmpeg for ${CPU} is completed"
 }
 
 #armv8-a
 ARCH=arm64
 CPU=armv8-a
-CC=$TOOLCHAIN/bin/aarch64-linux-android$API-clang
-CXX=$TOOLCHAIN/bin/aarch64-linux-android$API-clang++
-SYSROOT=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
-CROSS_PREFIX=$TOOLCHAIN/bin/aarch64-linux-android-
-PREFIX=$OUTPUTPATH/$CPU
-OPTIMIZE_CFLAGS="-march=$CPU"
+CC=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/aarch64-linux-android${API}-clang++
+SYSROOT=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+CROSS_PREFIX=${TOOLCHAIN}/bin/aarch64-linux-android-
+PREFIX=${OUTPUTPATH}/${CPU}
+OPTIMIZE_CFLAGS="-march=${CPU}"
 build_android
 
 #armv7-a
 ARCH=arm
 CPU=armv7-a
-CC=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang
-CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++
-SYSROOT=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
-CROSS_PREFIX=$TOOLCHAIN/bin/arm-linux-androideabi-
-PREFIX=$OUTPUTPATH/$CPU
-OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU "
+CC=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang
+CXX=${TOOLCHAIN}/bin/armv7a-linux-androideabi${API}-clang++
+SYSROOT=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+CROSS_PREFIX=${TOOLCHAIN}/bin/arm-linux-androideabi-
+PREFIX=${OUTPUTPATH}/${CPU}
+OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=${CPU}"
 build_android
 
 #x86
 ARCH=x86
 CPU=x86
-CC=$TOOLCHAIN/bin/i686-linux-android$API-clang
-CXX=$TOOLCHAIN/bin/i686-linux-android$API-clang++
-SYSROOT=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
-CROSS_PREFIX=$TOOLCHAIN/bin/i686-linux-android-
-PREFIX=$OUTPUTPATH/$CPU
+CC=${TOOLCHAIN}/bin/i686-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/i686-linux-android${API}-clang++
+SYSROOT=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+CROSS_PREFIX=${TOOLCHAIN}/bin/i686-linux-android-
+PREFIX=${OUTPUTPATH}/${CPU}
 OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
 build_android
 
 #x86_64
 ARCH=x86_64
 CPU=x86-64
-CC=$TOOLCHAIN/bin/x86_64-linux-android$API-clang
-CXX=$TOOLCHAIN/bin/x86_64-linux-android$API-clang++
-SYSROOT=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
-CROSS_PREFIX=$TOOLCHAIN/bin/x86_64-linux-android-
-PREFIX=$OUTPUTPATH/$CPU
-OPTIMIZE_CFLAGS="-march=$CPU -msse4.2 -mpopcnt -m64 -mtune=intel"
+CC=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang
+CXX=${TOOLCHAIN}/bin/x86_64-linux-android${API}-clang++
+SYSROOT=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+CROSS_PREFIX=${TOOLCHAIN}/bin/x86_64-linux-android-
+PREFIX=${OUTPUTPATH}/${CPU}
+OPTIMIZE_CFLAGS="-march=${CPU} -msse4.2 -mpopcnt -m64 -mtune=intel"
 build_android
