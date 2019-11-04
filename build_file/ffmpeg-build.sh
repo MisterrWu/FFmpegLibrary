fd3415fa54aa6e0afd31beab3c6bf5e6b0edb1a2
wuhan <han.wu-app@lumiunited.com>
2019-11-04 21:25:36 +0800

    Feature: edit build file with ffmpeg-build.sh

--- build_file/ffmpeg-build.sh
+++ build_file/ffmpeg-build.sh
@@ -3,7 +3,10 @@
 OUTPUTPATH=android_libs
 NDK=/Users/wuhan/Library/Android/sdk/ndk
 TOOLCHAIN=${NDK}/toolchains/llvm/prebuilt/darwin-x86_64
-API=29
+API=21
+
+MIN_PLATFORM=${NDK}/platforms/android-${API}
+ADDI_LDFLAGS="-Wl,-rpath-link=$MIN_PLATFORM/arch-arm/usr/lib -L$MIN_PLATFORM/arch-arm/usr/lib -nostdlib"
 
 function build_android
 {
@@ -30,8 +33,8 @@ echo "Compiling FFmpeg for $CPU"
     --target-os=android \
     --arch=${ARCH} \
     --cpu=${CPU} \
-    --cc=${CC}
-    --cxx=${CXX}
+    --cc=${CC} \
+    --cxx=${CXX} \
     --enable-cross-compile \
     --sysroot=${SYSROOT} \
     --extra-cflags="-Os -fpic ${OPTIMIZE_CFLAGS}" \
@@ -86,3 +89,66 @@ CROSS_PREFIX=${TOOLCHAIN}/bin/x86_64-linux-android-
 PREFIX=${OUTPUTPATH}/${CPU}
 OPTIMIZE_CFLAGS="-march=${CPU} -msse4.2 -mpopcnt -m64 -mtune=intel"
 build_android
+
+
+NDK=/Users/wuhan/Library/Android/sdk/ndk # NDK目录，自行修改
+API=21
+# arm aarch64 i686 x86_64  进行修改
+ARCH=aarch64
+# armv7a aarch64 i686 x86_64  进行修改
+PLATFORM=aarch64
+TARGET=$PLATFORM-linux-android
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin # 这里找到对应得文件
+SYSROOT=$NDK/platforms/android-${API}/arch-arm64
+PREFIX=${OUTPUTPATH}/${ARCH}
+
+CFLAG="-D__ANDROID_API__=$API -U_FILE_OFFSET_BITS -DBIONIC_IOCTL_NO_SIGNEDNESS_OVERLOAD -Os -fPIC -DANDROID -D__thumb__ -mthumb -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -marm"
+
+build_one()
+{
+echo "Compiling FFmpeg for $ARCH"
+./configure \
+--ln_s="cp -rf" \
+--prefix=$PREFIX \
+--cc=$TOOLCHAIN/$TARGET$API-clang \
+--cxx=$TOOLCHAIN/$TARGET$API-clang++ \
+--ld=$TOOLCHAIN/$TARGET$API-clang \
+--target-os=android \
+--arch=$ARCH \
+--cross-prefix=$TOOLCHAIN/$ARCH-linux-android- \
+--disable-asm \
+--enable-cross-compile \
+--enable-shared \
+--disable-static \
+--enable-runtime-cpudetect \
+--disable-doc \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-doc \
+--disable-symver \
+--enable-small \
+--enable-gpl --enable-nonfree --enable-version3 --disable-iconv \
+--enable-jni \
+--enable-mediacodec \
+--disable-decoders --enable-decoder=vp9 --enable-decoder=h264 --enable-decoder=mpeg4 --enable-decoder=aac \
+--disable-encoders --enable-encoder=h264_v4l2m2m \
+--disable-demuxers --enable-demuxer=rtsp --enable-demuxer=rtp --enable-demuxer=flv --enable-demuxer=h264 \
+--disable-muxers --enable-muxer=rtsp --enable-muxer=rtp --enable-muxer=flv --enable-muxer=h264 \
+--disable-parsers --enable-parser=mpeg4video --enable-parser=aac --enable-parser=h264 --enable-parser=vp9 \
+--disable-protocols --enable-protocol=rtmp --enable-protocol=rtp --enable-protocol=tcp --enable-protocol=udp \
+--disable-bsfs \
+--disable-indevs --enable-indev=v4l2 \
+--disable-outdevs \
+--disable-filters \
+--disable-postproc \
+--extra-cflags="$CFLAG" \
+--extra-ldflags="-marm"
+
+make clean
+make
+make install
+echo "The Compilation of FFmpeg for ${ARCH} is completed"
+}
+
+build_one
